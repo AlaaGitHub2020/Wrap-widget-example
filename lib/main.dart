c0ac6f7be7c91f43ca70b73ecbe7d47dc52c96da
Alaa Mashkouk <alaamashkouk2@gmail.com>
2020-05-28 15:13:42 +0530

    Warp Widget example

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:wrapwidgetexample/my_home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Wrap Widget Example',
+    home: MyHome(),
+  ));
+}
